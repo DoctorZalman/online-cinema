a74ccd25aac8f5027623c582bc0c82fa19c715f3
doctor_zalman <georgesun30@gmail.com>
2022-02-12 20:54:43 +0200

    navbar

--- src/components/Main-slider/MainSlider.tsx
+++ src/components/Main-slider/MainSlider.tsx
@@ -7,10 +7,10 @@ const MainSlider = () => {
             <div className='main_container'>
                 <div className="main-slider">
                     <div className="slider-items">
-                        <img src="../../img/slider1.jpg" alt="img-poster"/>
-                        <img src="../../img/slider2.jpg" alt="img-poster"/>
-                        <img src="../../img/slider3.jpg" alt="img-poster"/>
-                        <img src="../../img/slider4.jpg" alt="img-poster"/>
+                        <div className='item'></div>
+                        <div className='item'></div>
+                        <div className='item'></div>
+                        <div className='item'></div>
                     </div>
                 </div>
             </div>
