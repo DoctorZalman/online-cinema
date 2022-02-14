fd4d14156a98e9f8119330635cc2d1b11d7d44a0
doctor_zalman <georgesun30@gmail.com>
2022-02-14 18:21:43 +0200

    navbar style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import './styles/style.css';
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-import Navbar from "./components/Navbar/Navbar";
-import MainSlider from "./components/Main-slider/MainSlider";
-import SearchBar from "./components/SearchBar/SearchBar";
 import {Route, Routes} from "react-router-dom";
 import HomePage from "./components/Pages/Home/HomePage";
 import MoviesPage from "./components/Pages/Movies/MoviesPage";
