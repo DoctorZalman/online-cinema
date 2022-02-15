6852dbed2169d5687103573c64224828bb6405d6
doctor_zalman <georgesun30@gmail.com>
2022-02-15 14:52:30 +0200

    main slider

--- src/components/MainSlider/MainSlider.tsx
+++ src/components/MainSlider/MainSlider.tsx
@@ -1,14 +1,56 @@
 import React from "react";
-import './MainSlider.css'
-import DefaultSlider from "../DefaultSlider/Default-Slider";
-const MainSlider = () => {
+import './MainSlider.css';
+import Slider from "react-slick";
+
+function MainSlider() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1
+    }
     return (
-        <div className='slider-container'>
+        <div className="slider-container">
             <div className='main_container'>
-                <DefaultSlider />
+                <div className="main-slider">
+                    <Slider {...settings}>
+                        <div className='item-slider'>
+                            <div className="slider-details">
+                                <div className="description">
+                                    <div className="category">
+                                        <span className='action-cat'>Action</span>
+                                        <span className='adventure-cat'>Advanture</span>
+                                        <span className='fantasy-cat'>Fantasy</span>
+                                    </div>
+                                    <div className='movie-title'>guardians of the galaxy</div>
+                                    <div className="movie-details">
+                                        <span>Rating</span>
+                                    </div>
+                                    <div className="add-bnt">
+                                        <a href="#">Watch trailer</a>
+                                        <a href="#">Add to favorite</a>
+                                        <a href="#">Share</a>
+                                    </div>
+
+                                    <div className="btn btn-more-detail defaultButton">
+                                        <a href='#'>More detail</a>
+                                    </div>
+                                </div>
+                                <div className="poster">
+                                    {/*<img src="../../img/slider1.jpg" alt="poster"/>*/}
+                                </div>
+                            </div>
+                        </div>
+                        <div className='item-slider'>
+                            <div className="desc">1</div>
+                            <div className="poster">1</div>
+                        </div>
+                    </Slider>
+                </div>
             </div>
         </div>
     );
-};
+}
 
 export default MainSlider;
+
